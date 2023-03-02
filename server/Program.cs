964db89707771aa5a98a2ea96ad9fe81bf798fce
Tim Quigg <timjquigg@gmail.com>
2023-03-02 10:19:45 -0700

    Update build

--- server/Program.cs
+++ server/Program.cs
@@ -95,9 +95,9 @@ if (app.Environment.IsDevelopment())
 {
   app.UseSwagger();
   app.UseSwaggerUI();
+  app.UseHttpsRedirection();
 }
 
-app.UseHttpsRedirection();
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
